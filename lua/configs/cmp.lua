771f4d73a109e8f4edcc21d90bc1755f2d210cd9
jake zahm <jakezahm3@gmail.com>
2026-07-23 17:12:56 -0400

    ae355c31-7b0e-4e1f-b6bc-64d11e8ec678

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -1,13 +1,19 @@
-cmp.setup({
-    sources = {
-        { name = "codeium" }
-    }
-    formatting = {
-        format = require('lspkind').cmp_format({
-            mode = "symbol",
-            maxwidth = 50,
-            ellipsis_char = '...',
-            symbol_map = { Codeium = "", }
-        })
-    }
-})
+-- 1. Fetch NvChad's default configuration options table
+local options = require("nvchad.configs.cmp")
+
+-- 2. Safely inject your Codeium source into NvChad's list
+table.insert(options.sources, { name = "codeium", group_index = 2 })
+
+-- 3. Inject your custom lspkind icon formatting
+options.formatting = {
+  format = require("lspkind").cmp_format({
+    mode = "symbol",
+    maxwidth = 50,
+    ellipsis_char = "...",
+    symbol_map = { Codeium = "" },
+  }),
+}
+
+-- 4. Pass the modified table back to NvChad
+return options
+
--- lua/plugins/wsurf.lua
+++ lua/plugins/wsurf.lua
@@ -4,15 +4,28 @@ return {
     "Exafunction/windsurf.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp", -- Ensures nvim-cmp loads first
+      "hrsh7th/nvim-cmp",
     },
     config = function()
-      -- The internal engine namespace remains 'codeium' 
       require("codeium").setup({
-        -- Disable ghost text overlays so suggestions only show in your cmp popup
         enable_cmp_source = true,
-        virtual_text = { enabled = false },
+        virtual_text = { enabled = false }, -- Prevents duplicate ghost text
+        
+        -- Exact official configuration block for an external binary path
+        tools = {
+          language_server = "/home/jzahm/language_server_linux_x64",
+        },
       })
+    end,
+  },
+
+  -- 2. Modify NvChad's Built-in nvim-cmp Configuration inline
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local conf = require("nvchad.configs.cmp")
+      table.insert(conf.sources, { name = "codeium", group_index = 2 })
+      return conf
     end,
   },
 }
