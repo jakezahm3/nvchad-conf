8320911e1ab9778acddc1d427a4fb42e55d48e3a
jake zahm <jakezahm3@gmail.com>
2026-07-23 16:37:09 -0400

    df0d3606-0512-41a4-bf45-644c1e714cb9

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,37 +1,37 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre", -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  -- test new blink
-  { import = "nvchad.blink.lazyspec" },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-        "nix",
-        "python",
-        "rust",
-        "bash",
-        "yaml",
-        "json",
-      },
-    },
-  },
+	-- -- test new blink
+	-- { import = "nvchad.blink.lazyspec" },
+	--
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"vimdoc",
+				"html",
+				"css",
+				"nix",
+				"python",
+				"rust",
+				"bash",
+				"yaml",
+				"json",
+			},
+		},
+	},
 }
--- lua/plugins/wsurf.lua
+++ lua/plugins/wsurf.lua
@@ -1,12 +1,18 @@
 return {
-	{
-		"Exafunction/windsurf.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-		config = function()
-			require("codeium").setup({})
-		end,
-	},
+  -- 1. Free AI Engine (Windsurf / Codeium)
+  {
+    "Exafunction/windsurf.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp", -- Ensures nvim-cmp loads first
+    },
+    config = function()
+      -- The internal engine namespace remains 'codeium' 
+      require("codeium").setup({
+        -- Disable ghost text overlays so suggestions only show in your cmp popup
+        enable_cmp_source = true,
+        virtual_text = { enabled = false },
+      })
+    end,
+  },
 }
