f774cd9b91566e66cb6d4068624ebe5f16798f18
jake zahm <jakezahm3@gmail.com>
2026-07-23 11:04:56 -0400

    first commit3

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,21 +1,22 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    python = { "black" },
+     python = { "black" },
     rust = { "rustywind" },
     nix = { "alejandra" },
     bash = { "beautysh" },
     javascript = { "prettierd" },
     json = { "jq" },
     yaml = { "prettierd" },
+    css = { "prettier" },
+    html = { "prettier" },
   },
-}
 
-format_on_save = {
-  -- These options will be passed to conform.format()
-  timeout_ms = 500,
-  lsp_fallback = true,
-},
+   format_on_save = {
+     -- These options will be passed to conform.format()
+     timeout_ms = 500,
+     lsp_fallback = true,
+   },
 }
 
 return options
