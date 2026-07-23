214d9ec4626a5c739e3771d819cc1035d9e8eb4a
jake zahm <jakezahm3@gmail.com>
2026-07-23 19:47:33 -0400

    931114fe-4aed-4795-9423-ef3ac3e7bab1

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -1,13 +1,12 @@
 return {
-  -- Your existing plugins might be listed here...
-
-  -- Add the Windsurf client here:
-  {
-    "Exafunction/codeium.vim",
-    event = "BufReadPost",
-    config = function()
-      -- Force the plugin to run its server binary inside steam-run for NixOS
-      vim.g.codeium_wrapper = "steam-run"
-    end,
-  },
+	{
+		"Exafunction/windsurf.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			require("codeium").setup({})
+		end,
+	},
 }
