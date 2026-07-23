a060b06628e32f2bc5633fd5b02dc533ae37d21c
jake zahm <jakezahm3@gmail.com>
2026-07-23 09:31:01 -0400

    first commit3

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,11 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map({ "n", "v" }, "<leader>F", function()
+  require("conform").format({
+    lsp_fallback = true,
+    async = true,
+    timeout_ms = 500,
+  })
+end, { desc = "Format file or range (Conform)" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
