4e6239e0cd80e49801d69c672c84b99b1d91e1aa
jake zahm <jakezahm3@gmail.com>
2026-07-23 22:11:31 -0400

    7021bd06-c51c-4bc2-ab5e-27b30793e5cb

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -7,9 +7,7 @@ return {
 		},
 		config = function()
 			require("codeium").setup({
-				tools = {
-					language_server = "/home/jzahm/language_server_linux_x64",
-				},
+				wrapper = "steam-run"
 			})
 		end,
 	},
