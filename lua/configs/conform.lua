98d740feac64bd99db2d662482a36b24e75dbf07
jake zahm <jakezahm3@gmail.com>
2026-07-24 04:24:02 -0400

    fd694749-02b5-48b8-8351-952f9f08165a

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,22 +1,22 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "black" },
-    rust = { "rustywind" },
-    nix = { "alejandra" },
-    bash = { "beautysh" },
-     javascript = { "prettierd" },
-     json = { "jq" },
-     yaml = { "prettierd" },
-     css = { "prettier" },
-     html = { "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+		rust = { "rustywind" },
+		nix = { "alejandra" },
+		bash = { "beautysh" },
+		javascript = { "prettierd" },
+		json = { "jq" },
+		yaml = { "prettierd" },
+		css = { "prettier" },
+		html = { "prettier" },
+	},
 
-     format_on_save = {
-     -- These options will be passed to conform.format()
-     timeout_ms = 500,
-     lsp_fallback = true,
-   },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 }
 
 return options
