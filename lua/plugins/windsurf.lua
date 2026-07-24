be0b1067216d6c3373d51f475a20e09c44d9f2e3
jake zahm <jakezahm3@gmail.com>
2026-07-24 05:01:31 -0400

    acac9545-3453-4a8f-8449-78bc5da13d39

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -1,21 +1,19 @@
 return {
-	{
-		"Exafunction/windsurf.nvim",
-		event = "BufEnter",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"saghen/blink.cmp",
-		},
-		config = function()
-			require("codeium").setup({
-				tools = {
-					uuidgen = "/run/current-system/sw/bin/uuidgen",
-					gzip = "/run/current-system/sw/bin/gzip",
-					uname = "/run/current-system/sw/bin/uname",
-					curl = "/run/current-system/sw/bin/curl",
-					language_server = "/home/jzahm/language_server_linux_x64",
-				},
-			})
-		end,
+	"Exafunction/windsurf.nvim",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"saghen/blink.cmp",
 	},
+	config = function()
+		require("codeium").setup({
+			tools = {
+				uuidgen = "/run/current-system/sw/bin/uuidgen",
+				gzip = "/run/current-system/sw/bin/gzip",
+				uname = "/run/current-system/sw/bin/uname",
+				curl = "/run/current-system/sw/bin/curl",
+				language_server = "/home/jzahm/language_server_linux_x64",
+			},
+		})
+	end,
 }
