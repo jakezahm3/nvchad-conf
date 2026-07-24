3207e379d9d5be802a770efbfe2af97711b85e08
jake zahm <jakezahm3@gmail.com>
2026-07-23 22:35:16 -0400

    1f0ef3e3-d1dc-4203-8445-bf9b4f7720d5

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -1,13 +1,16 @@
 return {
 	{
 		"Exafunction/windsurf.nvim",
+		-- Lazy-loads on entering a buffer to ensure NvChad's environment variables settle
+		event = "BufEnter",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"hrsh7th/nvim-cmp",
 		},
 		config = function()
 			require("codeium").setup({
-				wrapper = "steam-run"
+				-- Corrected: wrapper sits cleanly at the top level of the setup table
+				wrapper = "steam-run",
 			})
 		end,
 	},
