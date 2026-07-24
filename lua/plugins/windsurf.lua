fa62a6c92479618c6b36d7699257ca0a91987374
jake zahm <jakezahm3@gmail.com>
2026-07-23 20:12:54 -0400

    dc6c860b-de4e-43fd-a045-21014c0c9179

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -1,12 +1,16 @@
-return {
-	{
-		"Exafunction/windsurf.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-		config = function()
-			require("codeium").setup({})
-		end,
-	},
+return {  -- Your other pre-existing NvChad starter plugins...
+  {
+    "Exafunction/windsurf.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({
+        -- This explicitly instructs the Lua plugin framework to boot 
+        -- its server binary directly through your system's nix-ld wrapper script
+        wrapper = "sh -c 'exec \"$@\"'"
+      })
+    end
+  },
 }
