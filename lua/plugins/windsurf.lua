e4a74981c876adcf511c4499109f62eb5c728e24
jake zahm <jakezahm3@gmail.com>
2026-07-23 23:44:24 -0400

    203213b9-afcf-47c5-bd90-33e69a305775

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -1,22 +1,14 @@
 return {
 	{
-		"Exafunction/codeium.nvim", -- Changed from windsurf.nvim
-		-- Lazy-loads on entering a buffer to ensure NvChad's environment variables settle
-		event = "BufEnter",
+		"Exafunction/windsurf.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"hrsh7th/nvim-cmp",
 		},
 		config = function()
-			require("codeium").setup({
-				-- Keeps your NixOS compatibility working perfectly
-				language_server = {
-					"/home/jzahm/language_server_linux_x64",
-				},
-				wrapper = "steam-run",
-				tools = {},
-				enable_chat = true, -- Optional: Enables the :Codeium Chat interface
-			})
+			require("codeium").setup( opts = {
+      bin_path = "/home/jzahm/Desktop/"
+      })
 		end,
 	},
 }
