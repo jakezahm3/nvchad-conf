f2ec88178a56e7489cf50219412cb498512da55c
jake zahm <jakezahm3@gmail.com>
2026-07-23 22:47:41 -0400

    52565af0-93a4-46b6-ae74-dfdbbaed7978

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -1,5 +1,7 @@
-cmp.setup({
-	sources = {
-		{ name = "codeium" },
-	},
-})
+local cmp = require("cmp")
+local conf = require("nvchad.configs.cmp")
+
+-- Insert codeium at the top of the existing sources list
+table.insert(conf.sources, 1, { name = "codeium" })
+
+cmp.setup(conf)
